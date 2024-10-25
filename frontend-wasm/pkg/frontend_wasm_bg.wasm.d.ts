4208b7351481f8ea10bdb64380123926195c05be
Dimitri Prosper <dimitri.prosper@gmail.com>
2024-10-25 16:47:12 -0400

    cleared pkg folder

--- frontend-wasm/pkg/frontend_wasm_bg.wasm.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function compare_cidr_networks(a: number, b: number, c: number, d: number): number;
-export function get_cidr_details(a: number, b: number): number;
-export function __wbindgen_malloc(a: number, b: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
-export function __wbindgen_exn_store(a: number): void;
